65475660dfdfea5ea57a5c3fc024f647b0c91aaf
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-09-21 11:28:45 +0100

    guests class

--- guests.rb
+++ guests.rb
@@ -2,8 +2,10 @@ class Guests
 
   attr_accessor
 
-  def initialize()
-
+  def initialize( guest_name, favourite_song, wallet )
+    @guest_name = guest_name
+    @favourite_song = favourite_song
+    @wallet = wallet
   end
-  
+
 end
--- karaoke_bar.rb
+++ karaoke_bar.rb
@@ -2,7 +2,8 @@ require_relative 'rooms'
 
 class KaraokeBar
 
-  attr_accessor :money_in_till, :room_space, :number_of_rooms
+  attr_accessor :money_in_till, :room_space,
+  attr_reader :number_of_rooms
 
   def initialize(money_in_till, number_of_rooms)
     @money_in_till = 1000
